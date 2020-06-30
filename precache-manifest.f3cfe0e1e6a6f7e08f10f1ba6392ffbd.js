8fcaac5db02a250806af7010448640735afd9159
mucoo2762 <dmsruf2762@gmail.com>
2020-06-30 10:46:29 +0900

    Updates

---
+++ precache-manifest.f3cfe0e1e6a6f7e08f10f1ba6392ffbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f28f72320803f3660b3a960d49ac8c",
+    "url": "/my_spa/index.html"
+  },
+  {
+    "revision": "b021ce0148f2a1e80d9c",
+    "url": "/my_spa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28ce764408eba2c0f2bd",
+    "url": "/my_spa/static/js/2.9d2d51e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_spa/static/js/2.9d2d51e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b021ce0148f2a1e80d9c",
+    "url": "/my_spa/static/js/main.9e320c61.chunk.js"
+  },
+  {
+    "revision": "77b654de03f1adc1d420",
+    "url": "/my_spa/static/js/runtime-main.f9b4ccf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_spa/static/media/logo.5d5d9eef.svg"
+  }
+]);
